800179a6325c2a626546ae4c02b095ad237a609f
Tsep1l0v <w1nd0wstsepilov@yandex.ru>
2024-10-16 16:11:26 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Дмитрий Цепилов on 16.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTTests/ChatGPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTTests.swift
+//  ChatGPTTests
+//
+//  Created by Дмитрий Цепилов on 16.10.2024.
+//
+
+import Testing
+@testable import ChatGPT
+
+struct ChatGPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
